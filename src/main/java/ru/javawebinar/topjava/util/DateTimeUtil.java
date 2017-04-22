707f2d4e995b370d1b4399d7714857e2f0cdcddb
petrovale <leha.isakov@yandex.ru>
2017-04-22 12:53:21 +0300

    HW01_add_servlet_meals

---
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.util;
+
+        import java.time.LocalDateTime;
+        import java.time.LocalTime;
+        import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by user on 21.04.2017.
+ */
+public class DateTimeUtil {
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+    }
+
+    public static String toString(LocalDateTime ldt) {
+        return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by user on 21.04.2017.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.info("getAll");
+        request.setAttribute("meals", MealsUtil.getWithExceeded(MealsUtil.MEALS, MealsUtil.DEFAULT_CALORIES_PER_DAY));
+        request.getRequestDispatcher("meals.jsp").forward(request, response);
+
+//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
+    }
+}
