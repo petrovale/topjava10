1e5377ad97c67ef6faa03889c1016c031d290d64
petrovale <leha.isakov@yandex.ru>
2017-05-13 22:23:34 +0300

    HW5_optional_fetch_join

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -27,4 +27,7 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     @Query("SELECT m FROM Meal m " +
             "WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
+
+    @Query("SELECT m FROM Meal m JOIN FETCH m.user WHERE m.id = ?1 AND m.user.id = ?2")
+    Meal getWithUser(int id, int userId);
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -4,10 +4,28 @@ package ru.javawebinar.topjava.service.datajpa;
  * Created by user on 13.05.2017.
  */
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(Profiles.DATAJPA)
 public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+
+    @Test
+    public void testGetByWitchMeals() throws Exception {
+        User user = service.getWithMeals(USER_ID);
+        MATCHER.assertEquals(USER, user);
+        MealTestData.MATCHER.assertCollectionEquals(MealTestData.MEALS, user.getMeals());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetByWitchMealsNotFound() throws Exception {
+        service.getWithMeals(1);
+    }
 }
